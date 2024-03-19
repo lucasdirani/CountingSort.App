c3c473f281a82bc85bb11d01c6217ad98b07f743
Lucas Dirani <lruizd@protonmail.com>
2024-03-19 07:45:04 -0300

    Adicionar arquivos de projeto.

---
+++ Algoritmos/AlgoritmoCountSort.cs
@@ -0,0 +1,33 @@
+﻿namespace CountingSort.App.Algoritmos
+{
+    public static class AlgoritmoCountSort
+    {
+        public static void CountSort(int[] arr, int n)
+        {
+            int[] saida = new int[n];
+            int intervalo = arr[0];
+            for (int i = 1; i < n; i++)
+            {
+                intervalo = Math.Max(intervalo, arr[i]);
+            }
+            int[] count = new int[intervalo + 1];
+            for (int i = 0; i < n; i++)
+            {
+                ++count[arr[i]];
+            }
+            for (int i = 1; i <= intervalo; i++)
+            {
+                count[i] += count[i - 1];
+            }
+            for (int i = 0; i < n; i++)
+            {
+                saida[count[arr[i]] - 1] = arr[i];
+                --count[arr[i]];
+            }
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = saida[i];
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,4 @@
+﻿using BenchmarkDotNet.Running;
+using CountingSort.App.Performance;
+
+_ = BenchmarkRunner.Run<TestePerformanceAlgoritmoCountSort>();
